d3311dd434fd416e67d1997c9f734d75bd5fc79c
Hassan Ali <7assan.3li1998@gmail.com>
2018-11-28 16:36:38 +0200

    first commit

---
+++ js/custom.js
@@ -0,0 +1,71 @@
+// PreLoader Script
+$(function(){
+    setTimeout(function(){$(".loader").fadeOut(800)},3600)
+});
+
+// Navbar Scrolling Script
+$(function(){
+    $(window).scroll(function(){
+        if(document.documentElement.scrollTop != 0){
+            $("nav").css({
+                "background":"var(--nav)",
+                "padding":"0px 30px",
+                "box-shadow":"1px 1px 6px rgba(0,0,0,.5)"
+            });
+        }
+        else {
+            $("nav").css({
+                "background":"none",
+                "padding":"20px 30px",
+                "box-shadow":"none"
+            });
+        }
+
+    });
+});
+
+// Open Colors Settings Script
+$(".settings button").click(function(){
+    $(this).siblings(".colors").toggleClass("closed");
+});
+
+// Apply Colors Settings Script
+$(".main").click(function(){
+    $(":root").css({
+        "--mc1":"#4D1D85",
+        "--mc2":"#d35400",
+        "--overlay":"rgba(78, 29, 133, 0.9)",
+        "--nav":"rgba(17, 0, 37, 0.9)"
+    });
+    $("header .header div input, header .header div select").css("box-shadow","1px 1px 10px rgba(0, 0, 0, 0.6)");
+});
+
+$(".red").click(function(){
+    $(":root").css({
+        "--mc1":"#c3200f",
+        "--mc2":"#02a081",
+        "--overlay":"#c32110de",
+        "--nav":"#bf2412e0"
+    });
+    $("header .header div input, header .header div select").css("box-shadow","1px 1px 10px rgba(0, 0, 0, 0.6)");
+});
+
+$(".bnw").click(function(){
+    $(":root").css({
+        "--mc1":"#000",
+        "--mc2":"#1995AD",
+        "--overlay":"rgba(0,0,0,0.9)",
+        "--nav":"rgba(0,0,0,0.95)"
+    });
+    $("header .header div input, header .header div select").css("box-shadow","1px 1px 10px rgba(255, 255, 255, 0.2)");
+});
+
+$(".green").click(function(){
+    $(":root").css({
+        "--mc1":"#137105",
+        "--mc2":"#262626",
+        "--overlay":"#137105e6",
+        "--nav":"#2d981ef2"
+    });
+    $("header .header div input, header .header div select").css("box-shadow","1px 1px 10px rgba(0, 0, 0, 0.6)");
+});
